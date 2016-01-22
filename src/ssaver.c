026aaa2e82357616728c75f26e24bbe252d497df
strippato <strippato@gmail.com>
2016-01-22 22:00:27 +0100

    screen saver

---
+++ src/ssaver.c
@@ -0,0 +1,51 @@
+/* gnome-arcade
+ * Copyright (c) 2014 Strippato strippato@gmail.com
+ *
+ * This file is part of gnome-arcade.
+ *
+ * gnome-arcade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gnome-arcade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gnome-arcade.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#include "global.h"
+#include "ssaver.h"
+
+void
+ssaver_init (void)
+{
+
+}
+
+void
+ssaver_free (void)
+{
+    
+}
+
+void
+ssaver_enable (void)
+{
+    // TODO
+    g_print ("TODO: screen saver enabled\n");
+}
+
+void
+ssaver_disable (void)
+{
+    // TODO
+    g_print ("TODO: screen saver disabled\n");
+}
