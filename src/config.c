35dc880534ee13bcd05e3d92e9826f8ae8ec8093
strippato <strippato@gmail.com>
2016-07-16 17:53:08 +0200

    mame: multithreading functionality has been removed

--- src/config.c
+++ src/config.c
@@ -40,7 +40,7 @@ cfg_fillDefaultConfig (void)
 {
 
 	g_hash_table_insert (cfg_default, g_strdup ("MAME_EXE"), g_strdup (MAME_BIN));
-	g_hash_table_insert (cfg_default, g_strdup ("MAME_OPTIONS"), g_strdup ("-autosave -skip_gameinfo -multithreading -video opengl"));
+	g_hash_table_insert (cfg_default, g_strdup ("MAME_OPTIONS"), g_strdup ("-autosave -skip_gameinfo -video opengl"));
 
 	g_hash_table_insert (cfg_default, g_strdup ("USE_DARK_THEME"), g_strdup ("1"));
 
